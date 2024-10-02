7e4b466ca198e0c49620de1fdddfe7ef7a162994
abraham <wole1702@gmail.com>
2024-10-02 04:51:17 +0100

    code

--- 0x00-pascal_triangle.py/0-pascal_triangle.py
+++ 0x00-pascal_triangle.py/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ python algorithm for a pascal triangle """
 
+
 def pascal_triangle(n):
     """ function return n number
         of the pascal triangle
@@ -14,12 +15,17 @@ def pascal_triangle(n):
     count = 0
     pascal = [[1]]
     while (count < n-1):
-        pascal[count].insert(0, 0) #inserting 0 beginning for a simpler approach
-        pascal[count].append(0) #inserting 0 at ending for a simpler approach
-        pas  = []
-        for i in range(len(pascal[count])-1): #iterate over the value of each list
-            pas.append(pascal[count][i] + pascal[count][i+1]) #adds the previous index to the next
+        pascal[count].insert(0, 0)
+        # inserting 0 beginning for a simpler approach
+        pascal[count].append(0)
+        # inserting 0 at ending for a simpler approach
+        pas = []
+        for i in range(len(pascal[count])-1):
+            # iterate over the value of each list
+            pas.append(pascal[count][i] + pascal[count][i+1])
+            # adds the previous index to the next
         pascal.append(pas)
-        pascal[count] = pascal[count][1:-1] #removes the initial zeros at beginning and end
+        pascal[count] = pascal[count][1:-1]
+        # removes the initial zeros at beginning and end
         count += 1
     return pascal
