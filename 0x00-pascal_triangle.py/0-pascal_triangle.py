b189a645e59d39b5b14f1478f59d2fe836730cb8
abraham <wole1702@gmail.com>
2024-10-02 04:45:57 +0100

    code

---
+++ 0x00-pascal_triangle.py/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" python algorithm for a pascal triangle """
+
+def pascal_triangle(n):
+    """ function return n number
+        of the pascal triangle
+    """
+
+    if n <= 0 or n is None:
+        return []
+    if n == 1:
+        return [[1]]
+
+    count = 0
+    pascal = [[1]]
+    while (count < n-1):
+        pascal[count].insert(0, 0) #inserting 0 beginning for a simpler approach
+        pascal[count].append(0) #inserting 0 at ending for a simpler approach
+        pas  = []
+        for i in range(len(pascal[count])-1): #iterate over the value of each list
+            pas.append(pascal[count][i] + pascal[count][i+1]) #adds the previous index to the next
+        pascal.append(pas)
+        pascal[count] = pascal[count][1:-1] #removes the initial zeros at beginning and end
+        count += 1
+    return pascal
