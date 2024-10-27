8ad6e7e69f39801fa89eece40ae88ad97879bec1
abraham <wole1702@gmail.com>
2024-10-27 15:21:08 +0100

    added

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,46 +22,57 @@ line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
 
 
 import sys
+import signal
+import os
+import re
 
-# store the count of all status codes in a dictionary
-status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                     '404': 0, '405': 0, '500': 0}
 
+status_codes = {"200": 0, "301": 0,
+                "400": 0, "401": 0,
+                "403": 0, "404": 0,
+                "405": 0, "500": 0}
+
+counter = 0
 total_size = 0
-count = 0  # keep count of the number lines counted
+
+
+def print_stats():
+    """ prints the stats for each status code """
+    print(f"File size: {total_size}")
+    for code in status_codes.keys():
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
+
+
+def handler(sig, frame):
+    """ sigint handler for 'Ctrl-C' """
+    print_stats()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, handler)
+
+pattern = (r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+           r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] '
+           r'"(GET /projects/\d+ HTTP/1\.1)" '
+           r'(\d{3}) (\d+)$')
+
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
+        counter += 1
+        match = re.match(pattern, line)
+        if match:
+            stat = line.split(" ")
+            if stat[-2] in status_codes.keys():
+                status_codes[stat[-2]] += 1
+                total_size += int(stat[-1])
+            else:
+                continue
+        if counter == 10:
+            print_stats()
+            counter = 0
 
-        if len(line_list) > 4:
-            status_code = line_list[-2]
-            file_size = int(line_list[-1])
-
-            # check if the status code receive exists in the dictionary and
-            # increment its count
-            if status_code in status_codes_dict.keys():
-                status_codes_dict[status_code] += 1
-
-            # update total size
-            total_size += file_size
-
-            # update count of lines
-            count += 1
-
-        if count == 10:
-            count = 0  # reset count
-            print('File size: {}'.format(total_size))
-
-            # print out status code counts
-            for key, value in sorted(status_codes_dict.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(status_codes_dict.items()):
-        if value != 0:
+except KeyboardInterrupt:
+    print_stats()
+    sys.exit(0)
